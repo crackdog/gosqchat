511347d8301fe80b33688bccd30b75efba928340
Florian Kahllund <flo.kahllund@gmail.com>
2014-05-03 10:58:18 +0200

    adding ts3serverquery error

--- ts3sq/ts3serverquery.go
+++ ts3sq/ts3serverquery.go
@@ -8,22 +8,68 @@ import (
 	"strings"
 )
 
-type Ts3sqs struct {
-	serverconn net.Conn
+var (
+	InvalidLoginError = NewError(520, "invalid loginname or password", "")
+)
+
+//Error contains an Error Message from the server
+type Error struct {
+	Id       int    //Error Id
+	Msg      string //Error Message
+	ExtraMsg string //An additional Error Message
 }
 
-func New(address string) (*Ts3sqs, error) {
+//Error returns an Error String
+func (err Error) Error() string {
+	if err.ExtraMsg == "" {
+		return fmt.Sprintf("error id=%d msg=%s", err.Id, err.Msg)
+	} else {
+		return fmt.Sprintf("error id=%d msg=%s extra_msg=%s", err.Id, err.Msg, err.ExtraMsg)
+	}
+}
+
+//NewError creates a new Error.
+func NewError(id int, msg, extraMsg string) Error {
+	return Error{id, msg, extraMsg}
+}
+
+//Ts3sqs contains a connection to a ts3server.
+type Ts3sqs struct {
+	serverconn net.Conn
+	WelcomeMsg string
+}
+
+//New create a new ts3serverquery connection.
+func New(address string) (t *Ts3sqs, err error) {
 	c, err := net.Dial("tcp", address)
 	if err == nil {
 		t := new(Ts3sqs)
 		t.serverconn = c
+		t.WelcomeMsg, err = t.WaitForMessageLine()
+		if err != nil {
+			c.Close()
+			return nil, err
+		}
+		s, err := t.WaitForMessageLine()
+		if err != nil {
+			c.Close()
+			return nil, err
+		}
+		t.WelcomeMsg += s
+		fmt.Println(t.WelcomeMsg) //logging...
 		return t, nil
 	} else {
 		return nil, err
 	}
 }
 
+//Close closes the connection to the ts3 server.
+func (s *Ts3sqs) Close() {
+	s.serverconn.Close()
+}
+
 func (s *Ts3sqs) send(msg string) error {
+	fmt.Printf("sending: '%s'\n", msg)
 	length, err := s.serverconn.Write([]byte(msg))
 	if err == nil && length < len(msg) {
 		return fmt.Errorf("only %d of %d bytes were sended.", length, len(msg))
@@ -34,7 +80,12 @@ func (s *Ts3sqs) send(msg string) error {
 
 func escape(s string) string {
 	//escape replaces all spaces with \s
-	return s
+	return strings.Replace(s, " ", "\\s", -1)
+}
+
+func unescape(s string) string {
+	//unescape replaces all \s with spaces
+	return strings.Replace(s, "\\s", " ", -1)
 }
 
 func (s *Ts3sqs) WaitForMessageLine() (string, error) {
@@ -51,7 +102,8 @@ func (s *Ts3sqs) getError() error {
 		if strings.Contains(msg, "error id=0 msg=ok") {
 			return nil
 		} else {
-			return fmt.Errorf("msg error: '%s'", strings.TrimSpace(msg))
+			//return fmt.Errorf("msg error: '%s'", unescape(strings.TrimSpace(msg)))
+			return NewError(0, unescape(strings.TrimSpace(msg)), "")
 		}
 	}
 }
@@ -69,7 +121,7 @@ func (s *Ts3sqs) Login(username, password string) error {
 	//logging in...
 	username = escape(username)
 	password = escape(password)
-	msg := fmt.Sprintf("login client_login_name=%s client_login_password=%s\n")
+	msg := fmt.Sprintf("login client_login_name=%s client_login_password=%s\n", username, password)
 	return s.sendWithGettingError(msg)
 }
 
@@ -85,7 +137,7 @@ func (s *Ts3sqs) Clientlist() (string, error) {
 
 func (s *Ts3sqs) Use(server_id int) error {
 	//Use sends a request to use a server.
-	msg := fmt.Sprintf("use sid=%d", server_id)
+	msg := fmt.Sprintf("use sid=%d\n", server_id)
 	return s.sendWithGettingError(msg)
 }
 
